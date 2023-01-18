db5936099efc5815357983a081141f5c5f46fc93
kenzodiac <kennethfujimura@gmail.com>
2023-01-18 15:52:34 -0800

    html skeleton construction; js variables declared

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+let homeBtn = document.getElementById("homeBtn");
+let rulesBtn = document.getElementById("rulesBtn");
+
+let mainStage = document.getElementById("mainStage");
+
+let modeSelect = document.getElementById("modeSelect");
+let twoPlayerBtn = document.getElementById("twoPlayerBtn");
+let vsCompBtn = document.getElementById("vsCompBtn");
+
+let lengthSelect = document.getElementById("lengthSelect");
+let bestOf1Btn = document.getElementById("bestOf1Btn");
+let bestOf5Btn = document.getElementById("bestOf5Btn");
+let bestOf7Btn = document.getElementById("bestOf7Btn");
+
+let rpslsInputs = document.getElementById("rpslsInputs");
+let outputArea = document.getElementsById("outputArea");
+let scoreBoard = document.getElementById("scoreBoard");
+let play1Title = document.getElementById("play1Title");
+let play1Score = document.getElementById("play1Score");
+let play2Title = document.getElementById("play2Title");
+let play2Score = document.getElementById("play2Score");
+
